f6f4fc16fc1f9b529f1ba3b949cef05c59f87924
Hyoeun Lee <gydms0103@naver.com>
2024-11-20 15:50:52 +0900

    Change PORT 2000 to 3000

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def remove_background():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 2000))
+    port = int(os.environ.get('PORT', 3000))
     app.run(debug=True, host='0.0.0.0', port=port)
